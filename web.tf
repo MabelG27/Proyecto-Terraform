f65e15261bf60e1f2e44840efca748e3146f7f1c
Mabel Garcia <mabelgarcia_d27@hotmail.com>
2022-10-16 22:23:07 -0400

    Terraform y DigitalOcean

--- web.tf
+++ web.tf
@@ -4,22 +4,37 @@ resource "digitalocean_droplet" "web" {
   region = "nyc1"
   size = "s-1vcpu-1gb"
   private_networking = true
-  ssh_keys = [
-    data.digitalocean_ssh_key.terraform.id
-  ]
+  user_data ="${file("userdata.yaml")}"
+  ssh_keys = [data.digitalocean_ssh_key.terraform.id]
+
+
+# Crear un repositorio.
+resource "digitalocean_droplet" "Git_TF" {
+  name = "Git_TF"
+}
+
   connection {
     host = self.ipv4_address
     user = "root"
     type = "ssh"
-    private_key = "${file(var.pvt_key)}"
+    #private_key = "${file(var.pvt_key)}"
+    private_key = ("${path.module}/mab_terr")
     timeout = "2m"
   }
+
+  provisioner "file" {
+    source      = "${path.module}/keys.ssh"
+    destination = "/root/keys.sh"
+  }
+
+
   provisioner "remote-exec" {
     inline = [
-      #"export PATH=$PATH:/usr/bin",
+      "export PATH=$PATH:/usr/bin",
       # install nginx
       "sudo apt-get update",
       "sudo apt-get -y install nginx"
     ]
   }
+          
 }
